a21fab5a413da8b37fa144e8cb7ecf77e6dbcd84
suzanmagic <suzanmagic@gmail.com>
2023-01-25 14:46:11 +0800

    add template

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,8 +19,9 @@ from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("user-home", TemplateView.as_view(template_name="home.html"), name="home"),
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('', include('blog.urls')),
 ]
